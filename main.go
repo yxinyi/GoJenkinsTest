27e623865c2989bfc24456ab585925fd3583822d
YxY <526924057@qq.com>
2021-02-28 11:43:13 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f1(in chan int) {
+	fmt.Println(<-in)
+}
+
+func main() {
+	out := make(chan int)
+	go f1(out)
+	out <- 2
+	time.Sleep(time.Second * 1)
+}
